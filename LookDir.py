cecd8379c94871d3dbb980a5dabc956c86b7a062
Archboot07 <victorfalllee@outlook.com>
2023-09-27 15:25:14 -0300

    Commit inicial

---
+++ LookDir.py
@@ -0,0 +1,113 @@
+import sys
+import requests
+import pyfiglet
+from termcolor import colored
+import whois
+from tqdm import tqdm
+import os.path
+
+### MENU SIMPLES 
+class Menu:
+    def menu_User(self):
+
+        print(colored('#' * 50, 'red'))
+        title = pyfiglet.figlet_format("LookDir", font="doom")
+        print(colored(title, 'red'))
+        print(colored("\t By: Arch_0x3f5", 'red'))
+        print(colored("\t Discord: Arch_0x3f5", 'blue'))
+        print(colored("\t GitHub: __arch_0x3f5", 'green'))
+        print(colored("\n\n[1] - Explorar Diretórios\n[2] - Reconhecimento\n[3] - Sair", "green"))
+        print(colored('#' * 50, 'red'))
+
+    #Verifica se o Caminho da wordlist esta correto 
+    def wordlist_local(self, path):
+        if os.path.exists(path):
+            return path
+        
+        else:
+            print(colored(f'\n\n>>>>>>>>>>O ARQUIVO{path} NAO EXISTE<<<<<<<<<<<<<<<\n\n.', 'red'))
+            return run.verificar_dir()
+
+
+    #Verifica se existe o diretorio, verificando se o codigo response e 200
+    def verificar_dir(self):
+        try:
+            #url do alvo 
+            self.url_alvo = input(colored("\nDIGITE A URL: ", 'green'))
+            print(colored('#' * 50, 'red'))
+
+            self.wordlist = input(colored("\n\nDIGITE O CAMINHO DA WORDLIST: ", 'green'))
+            self.wordlist = self.verificar_dir(self.wordlist)
+            print(colored('#' * 50, 'blue'))
+            print('\n\n')
+
+            #leitura da wordlist e cada dir em tempo real que esta sendo verificado 
+            with open(self.wordlist, 'r') as arquivos:
+                leitura_wordlist = arquivos.readlines()
+
+                for dir in tqdm(leitura_wordlist, desc="Verificando diretórios"):
+                    dir = dir.strip()
+
+                    sys.stdout.write("\r" + colored('Verificando:', 'red') + colored(f'{self.url}/{dir}', 'green'))
+                    sys.stdout.flush()
+
+                    response = requests.get(f'{self.url}/{dir}')
+
+                    if response.status_code == 200:
+                        print(colored(f'\n\n{self.url}/{dir}', 'blue') + colored(' ENCONTRADO', 'green') + '\n')
+                        print(colored('#' * 50, 'blue'))
+
+        except Exception as e:
+            print(colored("Erro:", 'red'), e)
+
+    #Busca de informaçoes ultilizando Whois
+    def buscar_info(self):
+        self.url = input(colored("\nURL ALVO: ", "green"))
+        busca = whois.whois(self.url)
+        print(colored("\nINFORMAÇÕES WHOIS:", 'green'))
+        print(colored(f"Data de Criação: {busca.creation_date[0] if isinstance(busca.creation_date, list) else busca.creation_date}", 'blue'))
+        print(colored(f"Nome de Registro: {busca.registrant_name}", 'blue'))
+        print(colored(f"Data de Expiração: {busca.expiration_date}", 'blue'))
+        print(colored(f"Contato do Registrante: {busca.registrant_contact}", 'blue'))
+
+
+        # Opção de voltar ou sair do programa após a verificação
+        while True:
+            try:
+                voltar = input(colored("\n\nDeseja voltar ao menu? (sim/não): ", "green"))
+                if voltar.lower() == "sim":
+                    return
+                elif voltar.lower() == "não":
+                    sys.exit()  # Encerra o programa
+                else:
+                    print(colored("Por favor, digite 'sim' ou 'não'.", "red"))
+            except Exception as e:
+                print(colored("Erro:", 'red'), e)
+
+
+    # inicializador menu e opões 
+    def run(self):
+        while True:
+            self.menu_User()
+            try:
+                self.opt = int(input(colored("\nDIGITE A OPÇÃO: ", "green")))
+                print(colored('#' * 50, 'red'))
+                if self.opt == 1:
+                    self.verificar_dir()
+
+                elif self.opt == 2:
+                    self.buscar_info()
+
+                elif self.opt == 3:
+                    print(colored("Obrigado por usar o LookDir! Até a próxima.", "green"))
+                    sys.exit()
+                else:
+                    print(colored("Digite uma opção válida, 1, 2 ou 3 ", 'red'))
+            except ValueError:
+                print(colored("Digite uma opção válida, 1, 2 ou 3 ", 'red'))
+
+
+
+if __name__ == "__main__":
+    run = Menu()
+    run.run()
